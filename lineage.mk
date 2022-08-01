fe703b90e258ec1122a76e8cccc02f46efea3e1b
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2022-08-01 11:53:11 +0900

    Massive source changes
    Added missing files and fixed a lots of error
    System starting to boot from this point

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,3 @@
-# Inherit some common stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/novel-common/device-common.mk)
 
